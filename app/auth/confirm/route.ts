954fbbe7b0746ab0ae5f0bfd1f0ee7aae5e8aa2d
Osman <oosman123@hotmail.com>
2026-02-28 15:34:13 -0500

    fix: correct Supabase client in auth confirm route

--- app/auth/confirm/route.ts
+++ app/auth/confirm/route.ts
@@ -1,30 +1,28 @@
-import { createClient } from "@/utils/supabase/create-server-client";
+import { createRouteHandlerClient } from "@/utils/supabase/route-client";
 import { type EmailOtpType } from "@supabase/supabase-js";
 import { redirect } from "next/navigation";
 import { type NextRequest } from "next/server";
 
-export async function GET(request: NextRequest) {
-  const { searchParams } = new URL(request.url);
+export async function GET(req: NextRequest) {
+  const supabase = createRouteHandlerClient();
+
+  const { searchParams } = new URL(req.url);
   const token_hash = searchParams.get("token_hash");
   const type = searchParams.get("type") as EmailOtpType | null;
   const next = searchParams.get("next") ?? "/";
 
-  if (token_hash && type) {
-    const supabase = createSupabaseServerClient();
-
-    const { error } = await supabase.auth.verifyOtp({
-      type,
-      token_hash,
-    });
-    if (!error) {
-      // redirect user to specified redirect URL or root of app
-      redirect(next);
-    } else {
-      // redirect the user to an error page with some instructions
-      redirect(`/auth/error?error=${error?.message}`);
-    }
+  if (!token_hash || !type) {
+    return redirect("/auth/error");
   }
 
-  // redirect the user to an error page with some instructions
-  redirect(`/auth/error?error=No token hash or type`);
+  const { error } = await supabase.auth.verifyOtp({
+    type,
+    token_hash,
+  });
+
+  if (error) {
+    return redirect("/auth/error");
+  }
+
+  return redirect(next);
 }
